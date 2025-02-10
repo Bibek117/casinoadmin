4da73816c74994c36bf3386908566aa5ffa0620b
Bibek117 <bibekangdembay@gmail.com>
2025-02-11 00:04:55 +0545

    admin login:

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,26 @@
+"use client"
 import { Header } from "@/components/layout/header"
 import { Sidebar } from "@/components/layout/sidebar"
+import { useAuth } from "@/context/AuthProvider"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter();
+  const {user,loading} = useAuth();
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login") 
+    }
+  }, [user, loading, router])
+
+  if (loading) return <div>Loading...</div> 
+
+  if (!user) return null 
   return (
     <div className="flex min-h-screen">
       <Sidebar className="w-64 hidden md:block" />
